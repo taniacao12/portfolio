ba1fc6a4e9423b7608e1c327a700eea55d80b428
Tania Cao <taniacao12@gmail.com>
2026-04-05 06:21:39 -0400

    add landing page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig({
+  site: 'https://taniacao.vercel.app/',
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
