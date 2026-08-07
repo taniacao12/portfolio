ed85f4f44afb0e9d268a8c593ecc8a6a257546ad
Tania Cao <taniacao12@gmail.com>
2026-08-07 17:55:33 -0400

    finalize rainbow school

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,16 +1,16 @@
 const galleryImgs = document.getElementsByClassName('galleryImg');
-const modal = document.getElementById('galleryModal');
-const content = modal.querySelector('#content');
-const modalPrev = content.children[0];
-const modalImg = content.children[1].querySelector('img');
-const modalCaption = content.children[1].querySelector('h3');
-const modalNext = content.children[2];
+const modal = document.querySelector('.modal');
+const buttons = modal.querySelectorAll('button');
+const prevBtn = buttons[0];
+const nextBtn = buttons[1];
+const image = modal.querySelector('figure img');
+const caption = modal.querySelector('h3');
 
 let index;
 
 function setContent(img) {
-    modalImg.src = img.src;
-    modalCaption.innerHTML = img.alt;
+    image.src = img.src;
+    caption.innerHTML = img.alt;
 }
 
 // -----------------------------
@@ -19,29 +19,31 @@ function setContent(img) {
 [...galleryImgs].forEach((img, i) => {
     img.addEventListener('click', () => {
         index = i;
-        modal.classList.toggle('active');
+        modal.classList.toggle('hide');
+        root.classList.toggle('hide-scrollbar');
         setContent(img);
     });
 });
 
-modalPrev.addEventListener('click', () => {
+prevBtn.addEventListener('click', () => {
     index -= 1;
     if (index < 0) index = galleryImgs.length - 1;
     setContent(galleryImgs[index]);
 });
 
-modalNext.addEventListener('click', () => {
+nextBtn.addEventListener('click', () => {
     index += 1;
     if (index == galleryImgs.length) index = 0;
     setContent(galleryImgs[index]);
 });
 
 modal.addEventListener('click', (e) => {
-    if (!(modalImg.contains(e.target) ||
-        modalCaption.contains(e.target) ||
-        modalPrev.contains(e.target) ||
-        modalNext.contains(e.target))) {
-        modal.classList.toggle('active');
+    if (!(image.contains(e.target) ||
+        caption.contains(e.target) ||
+        prevBtn.contains(e.target) ||
+        nextBtn.contains(e.target))) {
+        modal.classList.toggle('hide');
+        root.classList.toggle('hide-scrollbar');
     }
 });
 
@@ -49,11 +51,14 @@ modal.addEventListener('click', (e) => {
 // Keyboard handler
 // -----------------------------
 document.addEventListener('keydown', (e) => {
-    if (e.key === 'ArrowLeft') {
-        modalPrev.click();
-    } if (e.key === 'ArrowRight') {
-        modalNext.click();
-    } else if (e.key === 'Escape') {
-        modal.classList.toggle('active');
+    if (!modal.classList.contains('hide')) {
+        if (e.key === 'ArrowLeft') {
+            prevBtn.click();
+        } if (e.key === 'ArrowRight') {
+            nextBtn.click();
+        } else if (e.key === 'Escape') {
+            modal.classList.toggle('hide');
+            root.classList.toggle('hide-scrollbar');
+        }
     }
 });
