c7e7d9313322e5f72df9c63e4e6da1551152b621
Tania Cao <taniacao12@gmail.com>
2026-08-11 15:22:17 -0400

    update dates

--- src/scripts/script.ts
+++ src/scripts/script.ts
@@ -1,16 +1,20 @@
 function format(files: Record<string, any>, type: string) {
-    return Object.values(files).map((mod) => ({
-        order: mod.frontmatter.order,
-        type: type,
-        href: mod.url,
-        title: mod.frontmatter.title,
-        coverImage: mod.frontmatter.coverImage,
-    }));
+    return Object.values(files).map((mod) => {
+        const rawDate = mod.frontmatter.date;
+        const date = new Date(rawDate.replace(/\./g, "-"));
+
+        return {
+            date: date,
+            type: type,
+            href: mod.url,
+            title: mod.frontmatter.title,
+            coverImage: mod.frontmatter.coverImage,
+        }
+    });
 }
 
 export function getWorks() {
     const projects = format(import.meta.glob("@projects/*.astro", { eager: true }), "project");
     const publications = format(import.meta.glob("@publications/*.astro", { eager: true }), "publication");
-    const works = [...projects, ...publications].sort((a, b) => a.order - b.order)
-    return works;
+    return [...projects, ...publications].sort((a, b) => b.date.getTime() - a.date.getTime());
 }
